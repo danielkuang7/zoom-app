a2f4ced198e1dfa9c3819f08459ad8df56923341
Daniel Kuang <danielkuang7@github.com>
2021-03-22 08:15:44 -0400

    Moved socket.emit into myPeer.on

--- public/script.js
+++ public/script.js
@@ -5,10 +5,10 @@ const myPeer = new Peer(undefined, {
 })
 
 myPeer.on('open', id =>{
-    
+    socket.emit('join-room', ROOM_ID, id)
 })
 
-socket.emit('join-room', ROOM_ID, 10)
+
 
 socket.on('user-connected', userId => {
 
